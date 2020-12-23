efd0b58753be7eb37f0c0b73a87204b364639da9
Laura Stanley <laurastanley24@gmail.com>
2020-12-23 14:46:52 -0500

    added functionality and design

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const lotrApp = {};
 
 // assign an array value to each character id 
 
-lotrApp.id = "5cd99d4bde30eff6ebccfbe6";
+// lotrApp.id = "5cd99d4bde30eff6ebccfbe6";
+
 lotrApp.allIds = ["5cd99d4bde30eff6ebccfea0","5cd99d4bde30eff6ebccfc15", "5cd99d4bde30eff6ebccfd0d", "5cd99d4bde30eff6ebccfbe6", "5cd99d4bde30eff6ebccfd81", "5cd99d4bde30eff6ebccfd23", "5cd99d4bde30eff6ebccfe2e", "5cd99d4bde30eff6ebccfc7c", "5cd99d4bde30eff6ebccfc57"]
+
+lotrApp.allPhotos = ["./assets/gandalf.jpeg", "./assets/frodo.jpeg", "./assets/samwise.jpeg", "./assets/aragorn.jpeg", "./assets/legolas.png", "./assets/gimli.jpg", "./assets/merry.jpg", "./assets/pippin.jpg", "./assets/boromir.jpg"]
+
+
+// lotrApp.allMovieTitles = ["5cd95395de30eff6ebccde5c", "5cd95395de30eff6ebccde5b", "5cd95395de30eff6ebccde5d"]
 
 // API url 
 
-lotrApp.url = `https://the-one-api.dev/v2/character/${lotrApp.id}/quote`;
+lotrApp.url = `https://the-one-api.dev/v2/character/quote`;
 
 // API key 
 
@@ -18,19 +24,23 @@ lotrApp.apiKey = `ViE0UaamXONB47rhAQ-K`;
 
 lotrApp.getQuotes = () => {
     $.ajax({
-        url: lotrApp.url,
-            method: "GET",
-            dataType: "json",
-            headers: {
-                Authorization: `Bearer ${lotrApp.apiKey}`
-            }
-                }).then((result) => {
-                    $(".quote-container").empty()
-                    lotrApp.displayQuotes(result);
+    url: lotrApp.url,
+        method: "GET",
+        dataType: "json",
+        headers: {
+            Authorization: `Bearer ${lotrApp.apiKey}`
+        }
+    }).then((result) => {  
+        $(".quote-container").empty();
+        $(".photo-container").empty();
+        $(".movie-title-container").empty();
+        $(".random-quote-container").empty();
 
+        lotrApp.displayQuotes(result);    
+        
     });
-
 };
+
 
 // create a method to get the selected option value from the drop down menu
 
@@ -45,41 +55,94 @@ lotrApp.getValue = () => {
         const selection = $("option:selected").val()
 
         lotrApp.id = lotrApp.allIds[selection]
+        lotrApp.photo = lotrApp.allPhotos[selection]
 
         lotrApp.url = `https://the-one-api.dev/v2/character/${lotrApp.id}/quote`;
 
         lotrApp.getQuotes();
+
     })    
 };
+
+// create a method to generate a random quote
+lotrApp.randomQuoteGenerator = () => {
+
+  
+    $("#randomizer").on("click", function () {
+
+        const randomNumber = Math.floor(Math.random() * 9);
+
+        console.log(randomNumber)
+
+        lotrApp.id = lotrApp.allIds[randomNumber]
+        lotrApp.photo = lotrApp.allPhotos[randomNumber]
+
+        lotrApp.url = `https://the-one-api.dev/v2/character/${lotrApp.id}/quote`;
+
+        lotrApp.getQuotes();
+
+    })   
+
+
+}
+
 
 // display results to the page 
 
 lotrApp.displayQuotes = (quote) => {
 
+    let selectedQuote = [Math.floor(Math.random() * quote.docs.length)];
+
+    console.log(selectedQuote)
+
     //generate random quote from array values. 
 
-    let randomQuote = [Math.floor(Math.random() * quote.docs.length)];
- 
+    let whichMovie = quote.docs[selectedQuote].movie;
+
+    console.log(whichMovie)
+
+    if (whichMovie === "5cd95395de30eff6ebccde5d") {
+        let movie = `<p>The Fellowship of the Ring</p> `
+
+        $(".movie-title-container").append(movie)
+
+
+    } else if (whichMovie === "5cd95395de30eff6ebccde5b") {
+        let movie = `<p>The Two Towers</p> `
+
+        $(".movie-title-container").append(movie)
+
+    } else if (whichMovie === "5cd95395de30eff6ebccde5c") {
+        let movie = `<p>Return of the King</p> `
+
+        $(".movie-title-container").append(movie)
+
+    } else {
+        null
+    }
+
     // create the html to display quotes to the page
 
     const htmlToAppend = `
+        <h3>"${quote.docs[selectedQuote].dialog}"</h3>   
+    `
+    const photoToAppend = `
+        <img src=${lotrApp.photo} alt="">
+    `
 
-        <div class="quote-holder">
+    $(".quote-container").append(htmlToAppend)
+    $(".photo-container").append(photoToAppend)
 
-            <h3>"${quote.docs[randomQuote].dialog}"</h3>
-
-        </div>
-        `
-        $(".quote-container").append(htmlToAppend)
-
-        return quote.docs[randomQuote]
-    
+    return quote.docs[selectedQuote]
 };
+
+
    
 // create  app initialization method 
 
 lotrApp.init = function () {
     lotrApp.getValue();
+    lotrApp.randomQuoteGenerator();
 };
 
 // create document ready
